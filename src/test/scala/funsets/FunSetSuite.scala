5d7827c9e30f06929341400fa845551a9c3f9273
locdle <ducloc1803@gmail.com>
2015-05-22 11:07:40 -0700

    finish basic functions on sets implement and tests

--- src/test/scala/funsets/FunSetSuite.scala
+++ src/test/scala/funsets/FunSetSuite.scala
@@ -86,7 +86,7 @@ class FunSetSuite extends FunSuite {
    * Once you finish your implementation of "singletonSet", exchange the
    * function "ignore" by "test".
    */
-  ignore("singletonSet(1) contains 1") {
+  test("singletonSet(1) contains 1"){
     
     /**
      * We create a new instance of the "TestSets" trait, this gives us access
@@ -101,7 +101,7 @@ class FunSetSuite extends FunSuite {
     }
   }
 
-  ignore("union contains all elements") {
+  test("union contains all elements"){
     new TestSets {
       val s = union(s1, s2)
       assert(contains(s, 1), "Union 1")
@@ -109,4 +109,26 @@ class FunSetSuite extends FunSuite {
       assert(!contains(s, 3), "Union 3")
     }
   }
+
+  test("intersect contains 1 element"){
+    new TestSets {
+      val s = union(s1, s2)
+      val t = union(s1, s3)
+      val m = intersect(s, t)
+      assert(contains(m, 1), "Intersect 1")
+      assert(!contains(m, 2), "Intersect 2")
+      assert(!contains(m, 3), "Intersect 3")
+    }
+  }
+
+  test("diff contains 1 element") {
+    new TestSets {
+      val s = union(s1, s2)
+      val t = diff(s, s1)
+      assert(contains(t, 2), "Diff 2")
+      assert(!contains(t, 1), "Diff 1")
+      assert(!contains(t, 3), "Diff 3")
+    }
+  }
+
 }
